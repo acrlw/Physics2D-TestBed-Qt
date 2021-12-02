8d9c31166ccebd3a746d90855a35f6d431a398ad
ACRL <angryaccelerated@qq.com>
2021-12-02 19:54:48 +0800

    添加项目文件。

---
+++ Physics2D-TestBed-Qt/main.cpp
@@ -0,0 +1,12 @@
+﻿// Physics2D-TestBed-Qt.cpp: 定义应用程序的入口点。
+//
+
+
+#include <iostream>
+#include "dependencies/Physics2D/include/physics2d.h"
+int main()
+{
+	Physics2D::Vector2 a(1, 2);
+	std::cout << a.x << ", " << a.y << std::endl;
+	return 0;
+}
