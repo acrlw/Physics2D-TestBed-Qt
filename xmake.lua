a60aa41fba521a55151f734fce84057fc49b7053
ACRL <2359769683@qq.com>
2023-03-15 16:06:01 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,15 @@
 add_rules("mode.debug", "mode.release")
 set_languages("c++20")
-
+target("Physics2D")
+    set_kind("static")
+    add_files("Physics2D-TestBed-Qt/dependencies/Physics2D/*.cpp")
+    
 
 target("Physics2D-TestBed-Qt")
     add_rules("qt.application")
+    add_deps("Physics2D")
+    add_headerfiles("Physics2D-TestBed-Qt/dependencies/Physics2D/*.h")
     add_headerfiles("Physics2D-TestBed-Qt/include/*.h")
-    add_files("Physics2D-TestBed-Qt/dependencies/Physics2D/*.cpp")
     add_includedirs("Physics2D-TestBed-Qt/include")
     add_files("Physics2D-TestBed-Qt/source/*.cpp")
     add_files("Physics2D-TestBed-Qt/main.cpp")
